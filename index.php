8181c18cc0042756e6f0eac70fbf0a9902c08af4
tanar <tanar@10.9.8.100>
2019-01-31 08:06:24 +0100

    szamsorsolás

--- index.php
+++ index.php
@@ -7,6 +7,18 @@
     <body>
         Ez az index.php
         <?php
+			// lottóhúzás
+			$kihuzottak=0;
+			while($kihuzottak<5)
+			{
+				$sorsoltszam=rand(1,90);
+				if(!isset($tomb[$sorsoltszam]))
+				{
+					$tomb[$sorsoltszam]=1;
+					$kihuzottak++;
+				}
+			}
+			var_dump($tomb);
             echo "Itt már a php script fut";
         // put your code here
         ?>
