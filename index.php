931310e9de8a8f0f42ad4be8613277e1cfd315d0
tanar <tanar@10.9.8.100>
2019-01-31 07:55:12 +0100

    Alapváltozat

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        Ez az index.php
+        <?php
+        // put your code here
+        ?>
+    </body>
+</html>
