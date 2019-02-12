39e96b057910c8da7be8ea7debdc43682728498b
tanar <tanar@10.9.8.100>
2019-02-12 08:09:56 +0100

    sorsolas függvény

--- index.php
+++ index.php
@@ -7,18 +7,24 @@
     <body>
         Ez az index.php
         <?php
-			// lottóhúzás
-			$kihuzottak=0;
-			while($kihuzottak<5)
-			{
-				$sorsoltszam=rand(1,90);
-				if(!isset($tomb[$sorsoltszam]))
-				{
-					$tomb[$sorsoltszam]=1;
-					$kihuzottak++;
-				}
-			}
+            function sorsolas()
+            {
+                $kihuzottak=0;
+                while($kihuzottak<5)
+                {
+                        $sorsoltszam=rand(1,90);
+                        if(!isset($tomb[$sorsoltszam]))
+                        {
+                                $tomb[$sorsoltszam]=1;
+                                $kihuzottak++;
+                        }
+                }
+                return $tomb;
+                // var_dump($tomb);
+            }
+			$sorsolt=sorsolas();
 			var_dump($tomb);
+			// lottóhúzás
                         echo "Ez voltak a szám tömb";
             echo "Itt már a php script fut";
         // put your code here
