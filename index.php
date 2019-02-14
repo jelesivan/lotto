d3011092fc0952e5b61411a0d47485a2bf76f1fd
tanar <tanar@10.9.8.100>
2019-02-14 08:00:10 +0100

    függvény áthelyezése a fgv.php-be

--- index.php
+++ index.php
@@ -7,23 +7,9 @@
     <body>
         Ez az index.php
         <?php
-            function sorsolas()
-            {
-                $kihuzottak=0;
-                while($kihuzottak<5)
-                {
-                        $sorsoltszam=rand(1,90);
-                        if(!isset($tomb[$sorsoltszam]))
-                        {
-                                $tomb[$sorsoltszam]=1;
-                                $kihuzottak++;
-                        }
-                }
-                return $tomb;
-                // var_dump($tomb);
-            }
+
 			$sorsolt=sorsolas();
-			var_dump($tomb);
+			var_dump($sorsolt);
 			// lottóhúzás
                         echo "Ez voltak a szám tömb";
             echo "Itt már a php script fut";
